96d987127ec11406bf06a1e5d8993c0e54f4c2ec
Kiraandaja <kiraandaja@gmail.com>
2022-05-08 15:55:19 -0700

    Initial Commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+
+        }
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+
+        }
+        public static string Add(int number1, int number2, bool isTrue)
+        {
+            var dollarAmount = 0;
+            string response = "";
+            if (isTrue)
+            {
+                dollarAmount = number1 + number2;
+
+                if (dollarAmount == 1)
+
+                {
+                    response = $"{dollarAmount} dollar.";
+                }
+
+
+                else
+                {
+                    response = $"{dollarAmount} dollars.";
+                }
+
+            }
+            return response;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
